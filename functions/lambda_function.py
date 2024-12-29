c309db2b2b494f5a82762c77a01c8c0ea86937b6
Jhonatan <jmonzonllanos@gmail.com>
2024-12-29 17:52:43 -0500

    test canary-deploy-gha-8

--- functions/lambda_function.py
+++ functions/lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hola te saludo desde lambda aws - version 4!"
+        "body": "Hola te saludo desde lambda aws - version 5!"
     }
