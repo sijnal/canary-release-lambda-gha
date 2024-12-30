d9ad3f5ea8b0a2b23fd57a5702cd2a7134435d0e
Jhonatan <jmonzonllanos@gmail.com>
2024-12-30 15:12:37 -0500

    probando el uso de ejecucion para retornar error

--- functions/lambda_function.py
+++ functions/lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
-        "statusCode": 200
-        body": "Holaaaa, te saludo desde lambda - version 4!
+        "statusCode": 200,
+        "body": "Hola esta es mi version estable!"
     }
