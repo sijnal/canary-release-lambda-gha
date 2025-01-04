a01dfa3a82e41e3c7bbd7141b1fa9ce20698d9e6
JhontanSystem <jmonzonllanos@gmail.com>
2025-01-04 13:37:48 -0500

    test de extraccion de logs de la ultima lambda publicada con una version inestable 1

--- functions/lambda_function.py
+++ functions/lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hola esta es mi version estable 3!"
+        "body": "Hola esta es mi version inestable 5!"
     }
