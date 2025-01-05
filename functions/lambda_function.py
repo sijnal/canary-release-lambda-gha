9d4dd54a0cb9cc099b00bb33ab685e2e425d5137
JhontanSystem <jmonzonllanos@gmail.com>
2025-01-05 08:53:44 -0500

    subiendo nuevo tipo de regla eventbridge

--- functions/lambda_function.py
+++ functions/lambda_function.py
@@ -1,6 +1,6 @@
 def lambda_handler(event, context):
     return {
-        statusCode": 200
-        "body": "Hola esta es mi version estable 4!
+        "body": "Hola esta es mi version estable 2!"
+    }
     
     
