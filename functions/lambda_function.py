d6c5536aac55af59fd067cae741c1b57afe5e874
JhontanSystem <jmonzonllanos@gmail.com>
2025-01-05 16:12:27 -0500

    probando el analisis con un cron job de metricas de eventbrige - 7

--- functions/lambda_function.py
+++ functions/lambda_function.py
@@ -3,6 +3,6 @@ import json
 def lambda_handler(event, context):
     # TODO implement
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda versions - 6!')
+        'statusCode': 200
+        'body': jon.dumps('Hello from Lambda versions - 7!'
     }
